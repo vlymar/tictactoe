93d1fb9fb707d4e0a7d816fad36012ca3d2cd4b1
Victor Lymar <victor@launchdarkly.com>
2019-12-11 00:00:48 +0000

    implemented game

--- js/script.js
+++ js/script.js
@@ -1,8 +1,98 @@
+var currentPlayer = "O";
+var numTurns = 0;
+var gameOver = false;
+
+let tester = 1;
+
+function sucks() {
+    var tester = 2;
+}
+
+sucks()
+console.log(tester)
 
 
+function changeCurrentPlayer() {
+    console.log(currentPlayer)
+    if (currentPlayer === "O") {
+        currentPlayer = "X";
+    } else {
+        currentPlayer = "O";
+    }
+}
 
+function checkIfDraw() {
+    numTurns += 1;
 
+    if (numTurns === 9) {
+        console.log("its a draw");
+        gameOver = true;
 
+        $("h1").text("It's a draw!");
+    }
+}
+
+function checkIfCurrentPlayerHasThree(tileOne, tileTwo, tileThree) {
+    if ($(tileOne).text() === currentPlayer &&
+        $(tileTwo).text() === currentPlayer &&
+        $(tileThree).text() === currentPlayer) {
+           return true;
+       } else {
+           return false;
+       }
+}
+
+function checkVerticalWins() {
+    if (checkIfCurrentPlayerHasThree("#tile1", "#tile4", "#tile7") ||
+        checkIfCurrentPlayerHasThree("#tile2", "#tile5", "#tile8") ||
+        checkIfCurrentPlayerHasThree("#tile3", "#tile6", "#tile9")
+       ) {
+           return true;
+       } else {
+           return false;
+       }
+}
+
+function checkHorizontalWins() {
+        if (checkIfCurrentPlayerHasThree("#tile1", "#tile2", "#tile3") ||
+        checkIfCurrentPlayerHasThree("#tile4", "#tile5", "#tile6") ||
+        checkIfCurrentPlayerHasThree("#tile7", "#tile8", "#tile9")
+       ) {
+           return true;
+       } else {
+           return false;
+       }
+}
+
+function checkDiagonalWins() {
+            if (checkIfCurrentPlayerHasThree("#tile1", "#tile5", "#tile9") ||
+        checkIfCurrentPlayerHasThree("#tile3", "#tile5", "#tile7")
+       ) {
+           return true;
+       } else {
+           return false;
+       }
+}
+
+function checkIfCurrentPlayerWon() {
+    if (checkVerticalWins() || checkHorizontalWins() || checkDiagonalWins()) {
+        $("h1").text(currentPlayer + " Wins!")
+        gameOver = true;
+    }
+}
+
+function performLogic(buttonId, tileId) {
+    $(buttonId).hide();
+    $(tileId).text(currentPlayer)
+
+    if (!gameOver) {
+        checkIfCurrentPlayerWon()
+    }
+
+    checkIfDraw()
+
+    changeCurrentPlayer();
+}
 
 
 $("#button1").click(function() {
